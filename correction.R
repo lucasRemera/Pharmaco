18db34c795989dd9a92494f48e46b032b2667c59
lucas <lucas.michon@remera.fr>
2018-04-11 17:06:57 +0200

    benjamini correction

--- correction.R
+++ correction.R
@@ -7,7 +7,8 @@ Bonferroni=function(pvalue,alpha=0.05){
 Benjamini=function(pvalue,alpha=0.05){
   N=length(pvalue)
   psort=sort(pvalue)
-  ben=1/N+(0:(N-1))*alpha/N
+  ben=alpha/N+(0:(N-1))*alpha/N
   ps=ben[which(psort>ben)[1]]
+  print(ps)
   return(list(validate=which(pvalue<ps),alphaCorrige=ps))
 }
