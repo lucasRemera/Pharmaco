39fb80ffcc165148cce65127ebe8a20d4f7409c7
lucas <lucas.michon@remera.fr>
2018-06-21 11:41:45 +0200

    add bayesian methods from dumochel 1999, gibbs sampling + exact posterior distribution

---
+++ dumouchel.R
@@ -0,0 +1,119 @@
+###############
+# medicaments #
+###############
+
+####################
+## Gibbs sampling ##
+####################
+
+O=10 #observed cases
+E=1 #expected cases
+
+modelM<-"
+model{
+A1~dgamma(alpha1,beta1)
+A2~dgamma(alpha2,beta2)
+lambda<-P*A1+(1-P)*A2
+tau<-lambda*E
+O~dpois(tau)
+
+alpha1~dexp(.1)
+beta1~dexp(0.1)
+alpha2~dexp(.1)
+beta2~dexp(.1)
+P~dbeta(5,95)
+}
+"
+library(rjags)
+require(coda)
+modelM.spec<-textConnection(modelM)
+jags <- jags.model(modelM.spec,
+                   data = list('O' = O,
+                               'E' = E),
+                   inits = list(alpha1=.2,beta1=.1,alpha2=2,beta2=4,P=0.5),
+                   n.chains=4, 
+                   n.adapt=100)
+update(jags,1000)
+samps.coda <- coda.samples(jags,
+                           c('lambda','P'),
+                           n.iter=100000
+)
+plot((samps.coda[[1]][,c("P")]))
+quantile((samps.coda[[1]][,c("lambda")]),probs = c(.025,.5,.975))
+
+###################################
+## exact posterior distribution  ##
+###################################
+dq=function(x,alpha=1,beta=1,E=1){
+  #print((beta))
+  #return((1+(beta/E))**(-x)*(1+E/beta)**(-alpha)*gamma(alpha+x)/(gamma(alpha)*factorial(x))  )
+  A1=(1+(beta/E))**(-x)*(1+E/beta)**(-alpha)
+  A2=lgamma(alpha+x)-lgamma(alpha)-lfactorial(x)
+  return(A1*exp(A2))
+}
+
+pq=function(q,alpha=1,beta=1,E=1){
+  if(length(q)==1) return(sum(sapply(0:q, function(i) dq(i,alpha,beta,E))))
+  else return(sapply(q,function(qq) pq(qq,alpha,beta,E)))
+}
+
+rq=function(n,alpha=1,beta=1,E=1){
+  if(n==1){
+    t=runif(1,0,1)
+    nn=0
+    q=pq(nn,alpha,beta,E)
+    while(q<t){
+      ##nn2=nn+1
+      ##q2=pq(nn2,alpha,beta,E)
+      #if(q2<t) nn=nn2
+      #else if((q+q2)/2<t) nn=nn2
+      ##q=q2
+      
+      nn=nn+1
+      q=pq(nn,alpha,beta,E)
+    }
+    return(nn)
+  }
+  else return(replicate(n,{rq(1,alpha,beta,E)}))
+}
+
+
+rpi=function(n,alpha1=1,beta1=1,alpha2=1,beta2=1,P=.5){
+  P*rgamma(n,alpha1,beta1)+(1-P)*rgamma(n,alpha2,beta2)
+}
+
+rqn=function(n,E,O,alpha1=1,beta1=1,alpha2=1,beta2=1,P=.5){
+  #A1=0
+  #A2=0
+  #while(A1==0&A2==0){
+  A1=rq(n,alpha1,beta1,E)
+  A2=rq(n,alpha2,beta2,E)
+  #print(paste0(A1,"  ",A2))
+  qq = P*A1/(P*A1+(1-P)*A2)
+  #}
+  idx=which(A1==0&A2==0)
+  while(length(idx)>0){
+    AA1=rq(length(idx),alpha1,beta1,E)
+    AA2=rq(length(idx),alpha2,beta2,E)
+    A1[idx]=AA1
+    A2[idx]=AA2
+    idx=which(A1==0&A2==0)
+  }
+  qq = P*A1/(P*A1+(1-P)*A2)
+  return(qq)
+}
+
+a1=.2
+b1=.1
+a2=2
+b2=4
+pp=1/3
+E=10
+O=100
+rlambda=function(n,E,O,alpha1=1,beta1=1,alpha2=1,beta2=1,P=.5){
+  vqn=rqn(n,E,O,alpha1,beta1,alpha2,beta2,P)
+  #print(vqn)
+  return(rpi(n,alpha1+O,beta1+E,alpha2+O,beta2+E,vqn))
+}
+
+vlambda=rlambda(1000,E,O,a1,b1,a2,b2,pp)
